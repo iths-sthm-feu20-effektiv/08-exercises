52b15ec9eb98a11d8efe652761be9d8cba762b2b
David Andersson <lejonmanen@gmail.com>
2021-05-05 10:05:11 +0200

    Cleaned up default code. Exercise 6 in progress

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,19 @@
+
+const Menu = () => {
+	// state
+
+	let content = null
+
+	return (
+		<div>
+			<nav>
+				klickbara menyalternativ
+			</nav>
+			<main>
+				{content}
+			</main>
+		</div>
+	)
+}
+
+export default Menu
